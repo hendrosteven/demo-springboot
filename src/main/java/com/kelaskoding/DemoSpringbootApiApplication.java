3e973e80ebb7120434ed52ee378787de89d30994
Hendro Steven <hendro.steven@gmail.com>
2021-07-01 19:49:55 +0700

    add new endpoint

--- src/main/java/com/kelaskoding/DemoSpringbootApiApplication.java
+++ src/main/java/com/kelaskoding/DemoSpringbootApiApplication.java
@@ -2,12 +2,22 @@ package com.kelaskoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class DemoSpringbootApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringbootApiApplication.class, args);
+	}
+
+	@GetMapping
+	public String welcome(){
+		return "Welcome to my API version 1.0";
 	}
 
 }
